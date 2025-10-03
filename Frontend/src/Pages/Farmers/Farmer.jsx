f80a7d77bd9412d220f9c48f2e5fafa902eed3e8
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-10-03 17:52:46 +0300

    implemented farmer's page

--- Frontend/src/Pages/Farmers/Farmer.jsx
+++ Frontend/src/Pages/Farmers/Farmer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Spinner from '../../Components/Spinner'
 const Farmer = () => {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className='w-full h-screen flex items-center flex-col  justify-center'>
+        <h1 className="sm:text-5xl text-3xl mb-6 font-bold">
+          {localStorage.getItem("name")}  Welcome to Sky<span className='text-green-500'>A</span>cre
+        </h1>
+        <p>Preparing your Data</p>
+        <Spinner/>
     </div>
   )
 }
