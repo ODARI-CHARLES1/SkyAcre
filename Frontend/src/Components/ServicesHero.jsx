08d45f4980ed92a4d6792c0bac9c7023ed85fd0f
machariachristine254 <machariachristine254@gmail.com>
2025-10-04 23:49:37 +0300

    updated resources page

--- Frontend/src/Components/ServicesHero.jsx
+++ Frontend/src/Components/ServicesHero.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 const ServicesHero = () => {
   return (
     <>
-      <section class="text-gray-600 body-font">
-  <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    {/* <img class="lg:w-4/6 md:w-3/6 w-5/6 h-64 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/> */}
-
-    <div class="text-center lg:w-2/3 w-full">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Our Services</h1>
-      <p class="mb-8 leading-relaxed">Take a look at the plethora of services we have to offer</p>
-      
-    </div>
+  <section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <div className="text-center lg:w-2/3 w-full">
+      <h1 className="title-font sm:text-6xl text-4xl mb-4 font-medium text-gray-900">Our Services</h1>
+      <p className="mb-8 leading-relaxed">Take a look at the variety of services we have to offer</p>
+      </div>
+  <img 
+ className="w-full max-w-7xl h-[650px] mx-auto mb-10 object-cover object-center rounded-2xl shadow-2xl"
+  src="https://fnb.tech/wp-content/uploads/2025/03/Smart-Farming-Tools.jpeg"
+  alt="Drone flying over green farmland for analysis"
+  />
   </div>
-</section>
+  </section>
     </>
   )
 }
