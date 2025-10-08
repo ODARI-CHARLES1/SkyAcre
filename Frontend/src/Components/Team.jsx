f6937abef1ae936794b5cc879d2fb7bc7c2065e6
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-10-08 17:33:07 +0300

    update and added odari profile picture

--- Frontend/src/Components/Team.jsx
+++ Frontend/src/Components/Team.jsx
@@ -60,7 +60,7 @@ const Team = () => {
       </div>
       <div class="p-4 lg:w-1/5 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-70 object-cover object-center mb-4" src="https://i.pinimg.com/736x/ec/38/9d/ec389dd1a5f4a8f331151e46c8a6863f.jpg"/>
+          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-70 object-cover object-center mb-4" src="https://ik.imagekit.io/odaricharles1/skyAcre/odariKCharles.jpg?updatedAt=1759933925797"/>
           <div class="w-full">
             <h2 class="title-font font-medium text-lg text-gray-900">Charles Odari</h2>
             <h3 class="text-gray-500 mb-3">IOT Designer</h3>
