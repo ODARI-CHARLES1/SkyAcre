0e150e71b197a389a16a4357fbec4421c8c5a206
Storm00212 <paulmuyalikhams@gmail.com>
2026-03-03 21:19:46 +0300

    fixes

--- AI-Models/load_from_huggingface.py
+++ AI-Models/load_from_huggingface.py
@@ -12,7 +12,7 @@ import os
 
 # Available backend options are: "jax", "torch", "tensorflow".
 # Using tensorflow as it is listed in requirements.txt
-os.environ["KERAS_BACKEND"] = "tensorflow"
+os.environ["KERAS_BACKEND"] = "jax"
 # To use the Hugging Face Hub, a token needs to be available.
 # It can be set as an environment variable `HF_TOKEN`.
 # A .env file can be used to store this token.
